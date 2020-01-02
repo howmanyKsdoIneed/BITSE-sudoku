89f046fe4ef8b8ee6a1592d6954b425ed982ab5d
howmanyKsdoIneed <wuhaolei19990531@qq.com>
2020-01-02 13:36:57 +0800

    加入求解数独的方法

--- sudoku/sudoku.cpp
+++ sudoku/sudoku.cpp
@@ -9,6 +9,7 @@ using namespace std;
 /// <param name="pos">列号</param>
 int GetOffset(_In_range_(0, 40319) int topId, _pos_range_ int pos);
 
+
 Sudoku::Sudoku() :board{ {0} } {}
 
 Sudoku::Sudoku(_In_range_(0, 2903039) int id, _ans_range_ int upLeft) : board{ {0} }
@@ -167,4 +168,54 @@ int GetOffset(_In_range_(0, 40319) int topId, _pos_range_ int pos)
 		div *= i;
 	modulus = div * i;
 	return (topId % modulus) / div;
+}
+
+bool Sudoku::Solve(_pos_range_ int line, _pos_range_ int col)
+{
+	//确定本次调用要处理的空位的位置
+	if (line < 0 || line>8)
+		line = 0;
+	if (col < 0 || col>8)
+		col = 0;
+	if (board[line][col] != 0)
+	{
+		for (line = 0; line < 9; line++)
+			for (col = 0; col < 9; col++)
+				if (board[line][col] == 0)
+					goto foundSlot;
+	}
+	//运行至此表明已遍历棋盘上所有位置，未找到空位
+	//若数独合法，返回true
+	return IsValid(false);
+
+foundSlot:;
+
+	//确定当前空位可以填入的数字
+	bool bNumAllowed[10]{ false };
+	for (int i = 1; i < 10; i++)
+		bNumAllowed[i] = true;
+	for (int i = 0; i < 9; i++)	//行和列
+	{
+		bNumAllowed[board[i][col]] = false;
+		bNumAllowed[board[line][i]] = false;
+	}
+	const int subBeginL = line - (line % 3);
+	const int subBeginC = col - (col % 3);
+	for (int subL = 0; subL < 3; subL++)	//九宫格
+		for (int subC = 0; subC < 3; subC++)
+			bNumAllowed[board[subBeginL + subL][subBeginC + subC]] = false;
+
+	//向下递归
+	for (int i = 1; i < 10; i++)
+	{
+		if (bNumAllowed[i] == false)
+			continue;
+		board[line][col] = i;
+		if (Solve())	//向下递归的结果是得解，就返回得解，并保持棋盘状态不变
+			return 1;
+	}
+
+	//所有可能的数字都无解，还原棋盘并返回无解
+	board[line][col] = 0;
+	return false;
 }
