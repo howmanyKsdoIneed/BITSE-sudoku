517c6a4e856996a64799e00ce27db8e949decea3
howmanyKsdoIneed <wuhaolei19990531@qq.com>
2020-01-08 21:01:14 +0800

    Revert "Revert "添加解数独模块的单元测试""
    This reverts commit aa0dd431435a5442c7e37427681ecb6044c9bf90.

---
+++ test/module-solve/module-solve.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef BITSE_MODULETEST_SOLVE
+#define BITSE_MODULETEST_SOLVE
+#include <iostream>
+#include <fstream>
+#include "../../sudoku/solver.h"
+
+#endif // !BITSE_MODULETEST_SOLVE
