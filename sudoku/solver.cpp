741eb0a96c63b530f3f2a97521982c8d1eaca36a
howmanyKsdoIneed <wuhaolei19990531@qq.com>
2020-01-11 18:37:25 +0800

    剪枝优化求解算法

--- sudoku/solver.cpp
+++ sudoku/solver.cpp
@@ -61,46 +61,46 @@ bool Solver::Solve(_pos_range_ int line, _pos_range_ int col)
 	vector<int> prefitL, prefitC, prefitNum;
 
 	//搜索只有一个数可以填入的空位
-	for(int line=0;line<9;line++)
-		for(int col=0;col<9;col++)
-			if (puzzle.At(line, col) == 0 && fitCount[line][col] == 1)
+	for (int _line = 0; _line < 9; _line++)
+		for (int _col = 0; _col < 9; _col++)
+			if (puzzle.At(_line, _col) == 0 && fitCount[_line][_col] == 1)
 			{
 				for (int num = 1; num < 10; num++)
 				{
-					if (canFit[line][col][num])
+					if (canFit[_line][_col][num])
 					{
 						//填入数字
-						puzzle.At(line, col) = num;
-						fillL.push_back(line);
-						fillC.push_back(col);
+						puzzle.At(_line, _col) = num;
+						fillL.push_back(_line);
+						fillC.push_back(_col);
 						//维护可填数字列表
 						for (int i = 0; i < 9; i++)
 						{
-							if (canFit[i][col][num])
+							if (canFit[i][_col][num])
 							{
-								canFit[i][col][num] = false;
-								fitCount[i][col]--;
+								canFit[i][_col][num] = false;
+								fitCount[i][_col]--;
 								prefitL.push_back(i);
-								prefitC.push_back(col);
+								prefitC.push_back(_col);
 								prefitNum.push_back(num);
 							}
-							if (canFit[line][i][num])
+							if (canFit[_line][i][num])
 							{
-								canFit[line][i][num] = false;
-								fitCount[line][i]--;
-								prefitL.push_back(line);
+								canFit[_line][i][num] = false;
+								fitCount[_line][i]--;
+								prefitL.push_back(_line);
 								prefitC.push_back(i);
 								prefitNum.push_back(num);
 							}
 						}
 						for(int subL=0;subL<3;subL++)
 							for (int subC = 0; subC < 3; subC++)
-								if (canFit[line / 3 + subL][col / 3 + subC][num])
+								if (canFit[_line / 3 + subL][_col / 3 + subC][num])
 								{
-									canFit[line / 3 + subL][col / 3 + subC][num] = false;
-									fitCount[line / 3 + subL][col / 3 + subC]--;
-									prefitL.push_back(line / 3 + subL);
-									prefitC.push_back(col / 3 + subC);
+									canFit[_line / 3 + subL][_col / 3 + subC][num] = false;
+									fitCount[_line / 3 + subL][_col / 3 + subC]--;
+									prefitL.push_back(_line / 3 + subL);
+									prefitC.push_back(_col / 3 + subC);
 									prefitNum.push_back(num);
 								}
 
@@ -114,18 +114,23 @@ bool Solver::Solve(_pos_range_ int line, _pos_range_ int col)
 		line = 0;
 	if (col < 0 || col>8)
 		col = 0;
-	if (puzzle.At(line, col) != 0)
+	int leastFits = 10;
+	for (int tgtL = 0; tgtL < 9; tgtL++)
 	{
-		for (line = 0; line < 9; line++)
-			for (col = 0; col < 9; col++)
-				if (puzzle.At(line, col) == 0)
-					goto foundSlot;
-		//运行至此表明已遍历棋盘上所有位置，未找到空位
-		//若数独合法，表明得解，返回true
-		//否则，还原棋盘状态，返回false
-		if (IsValid(false))
+		for (int tgtC = 0; tgtC < 9; tgtC++)
+		{
+			if (puzzle.At(tgtL, tgtC) == 0 && fitCount[tgtL][tgtC] < leastFits)
+			{
+				line = tgtL;
+				col = tgtC;
+			}
+		}
+	}
+	if (puzzle.At(line, col) != 0)	//已遍历棋盘上所有位置，未找到空位
+	{
+		if (IsValid(false))	//若数独合法，表明得解，返回true
 			return true;
-		else
+		else				//否则，还原棋盘状态，返回false
 		{
 			for (int i = 0; i < fillL.size(); i++)
 				puzzle.At(fillL[i], fillC[i]) = 0;
@@ -138,31 +143,58 @@ bool Solver::Solve(_pos_range_ int line, _pos_range_ int col)
 		}
 	}
 
-foundSlot:;
-
-	//确定当前空位可以填入的数字
-	bool bNumAllowed[10]{ false };
-	for (int i = 1; i < 10; i++)
-		bNumAllowed[i] = true;
-	for (int i = 0; i < 9; i++)	//行和列
-	{
-		bNumAllowed[puzzle.At(i, col)] = false;
-		bNumAllowed[puzzle.At(line, i)] = false;
-	}
-	const int subBeginL = line - (line % 3);
-	const int subBeginC = col - (col % 3);
-	for (int subL = 0; subL < 3; subL++)	//九宫格
-		for (int subC = 0; subC < 3; subC++)
-			bNumAllowed[puzzle.At(subBeginL + subL, subBeginC + subC)] = false;
-
 	//向下递归
-	for (int i = 1; i < 10; i++)
+	vector<int> fitL, fitC;	//记录可填数字更改
+	for (int num = 1; num < 10; num++)
 	{
-		if (bNumAllowed[i] == false)
+		if (!canFit[line][col][num])
 			continue;
-		puzzle.At(line, col) = i;
+		//填入数字
+		puzzle.At(line, col) = num;
+		//维护可填入数字表
+		for (int i = 0; i < 9; i++)
+		{
+			if (canFit[i][col][num])
+			{
+				canFit[i][col][num] = false;
+				fitCount[i][col]--;
+				fitL.push_back(i);
+				fitC.push_back(col);
+			}
+			if (canFit[line][i][num])
+			{
+				canFit[line][i][num] = false;
+				fitCount[line][i]--;
+				fitL.push_back(line);
+				fitC.push_back(i);
+			}
+		}
+		for (int subL = 0; subL < 3; subL++)
+		{
+			for (int subC = 0; subC < 3; subC++)
+			{
+				if (canFit[line / 3 + subL][col / 3 + subC][num])
+				{
+					canFit[line / 3 + subL][col / 3 + subC][num] = false;
+					fitCount[line / 3 + subL][col / 3 + subC]--;
+					fitL.push_back(line / 3 + subL);
+					fitC.push_back(col / 3 + subC);
+				}
+			}
+		}
+
 		if (Solve())	//向下递归的结果是得解，就返回得解，并保持棋盘状态不变
 			return true;
+		else			//若没有得解，就还原可填入数字表
+		{
+			for (int i = 0; i < fitL.size(); i++)
+			{
+				canFit[fitL[i]][fitC[i]][num] = true;
+				fitCount[fitL[i]][fitC[i]]++;
+			}
+			fitL.clear();
+			fitC.clear();
+		}
 	}
 
 	//所有可能的数字都无解，还原棋盘并返回无解
